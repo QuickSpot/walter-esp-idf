d20fc1e64cb59b8a8a2ecb9a5ce190a8a3bb3758
Thibo Verheyde <thibo@dptechnics.com>
2024-10-16 10:15:48 +0200

    Fix typo in examples

--- examples/walter-modem/positioning/main/positioning.cpp
+++ examples/walter-modem/positioning/main/positioning.cpp
@@ -167,8 +167,8 @@ bool lteConnect()
 
   /* Wait for the network to become available */
   WalterModemNetworkRegState regState = modem.getNetworkRegState();
-  while(!(regState == WALTER_MODEM_NETOWRK_REG_REGISTERED_HOME ||
-          regState == WALTER_MODEM_NETOWRK_REG_REGISTERED_ROAMING))
+  while(!(regState == WALTER_MODEM_NETWORK_REG_REGISTERED_HOME ||
+          regState == WALTER_MODEM_NETWORK_REG_REGISTERED_ROAMING))
   {
     vTaskDelay(pdMS_TO_TICKS(1000));
     WalterModemRsp rsp;
