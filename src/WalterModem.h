61abeec14171000f076cbbf6de3efa20c0351eaa
Daan Pape <daanpape@users.noreply.github.com>
2025-02-21 09:35:29 +0100

    Update WalterModem.h
    Fixed some comments

--- src/WalterModem.h
+++ src/WalterModem.h
@@ -235,10 +235,10 @@
 #define BITCHAR(x, n) (((x) >> (n)) & 1 ? '1' : '0')
 
 /**
- * @brief Create a string on the heap that is a 0-terminated binary representation of a byte.
+ * @brief Compound literal which represents a byte as a 0-terminated bitstring.
  */
-#define BINBYTESTR(x) (char[]){BITCHAR(x, 7), BITCHAR(x, 6), BITCHAR(x, 5), BITCHAR(x, 4), \
-                               BITCHAR(x, 3), BITCHAR(x, 2), BITCHAR(x, 1), BITCHAR(x, 0), '\0'}
+#define BINBYTESTR(x) (char[9]){BITCHAR(x, 7), BITCHAR(x, 6), BITCHAR(x, 5), BITCHAR(x, 4), \
+                                BITCHAR(x, 3), BITCHAR(x, 2), BITCHAR(x, 1), BITCHAR(x, 0), '\0'}
 
 /**
  * @brief This enum groups status codes of functions and operational components
@@ -2890,12 +2890,13 @@ class WalterModem
          * @brief Converts a given duration to encoded uint8_t according to the base_times.
          *
          * This function will encode a the given duration according to the base_times / mulipliers for use in PSM.
+         *
          * @warning This is an approximation based on the base_times array.
          *
          * @param base_times Pointer to an array containing the base times
-         * @param base_times_len Lenght of the base_times array
-         * @param duration_seconds The duration in seconds
-         * @param actual_duration_seconds Pointer to where the actual duration in seconds should be filled in
+         * @param base_times_len Length of the base_times array
+         * @param duration_seconds The requested duration in seconds.
+         * @param actual_duration_seconds Optional pointer in which the actual requested duration can be saved.
          *
          * @return The duration encoded into the 3GPP standard format.
          */
@@ -4576,25 +4577,33 @@ class WalterModem
         static void offlineMotaUpgrade(uint8_t *otaBuffer);
 
         /**
-         * @brief Converts a given combination of sconds,minutes, hours to a TAU approximation
-         * @warning This function is an approximation because it uses a multiplier internally.
+         * @brief Encode a TAU duration for use in PSM configuration.
+         * 
+         * This function will encode a given duration into the nearest duration that can be encoded
+         * according to  the 3GPP specification for use in timer T3412 (TAU)
+         *
+         * @warning This function is an approximation because of the encoding used over the wire.
          *
          * @param seconds Duration in seconds
          * @param minutes Duration in minutes
          * @param hours  Duration in hours
-         * @param actual_duration_seconds Pointer to write the calulated total amount of seconds to
+         * @param actual_duration_seconds Optional pointer in which the actual requested duration can be saved.
          *
-         * @return The duration encoded into the 3GPP standard format.
+         * @return The interval encoded into the 3GPP standard format.
          */
         static const uint8_t durationToTAU(uint32_t seconds = 0, uint32_t minutes = 0, uint32_t hours = 0, uint32_t *actual_duration_seconds = nullptr);
 
         /**
          * @brief Converts a given duration of seconds, minutes to a reqActive approximation
+         *
+         * This function will encode a given duration into the nearest duration that can be encoded
+         * according to  the 3GPP specification for use in timer T3324 (active time).
+         *
          * @warning This function is an approximation because it uses a multiplier internally.
          *
          * @param seconds Duration in seconds
          * @param minutes Duration in minutes
-         * @param actual_duration_seconds Pointer to write the calulated total amount of seconds to
+         * @param actual_duration_seconds Optional pointer in which the actual requested duration can be saved.
          *
          * @return The duration encoded into the 3GPP standard format.
          */
