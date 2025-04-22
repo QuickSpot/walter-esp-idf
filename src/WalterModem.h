0c90cec499ce54d74d55a5285f90bfdb0937f5bc
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-04-22 15:02:51 +0200

    fix for #34 and #35

--- src/WalterModem.h
+++ src/WalterModem.h
@@ -3561,8 +3561,6 @@ class WalterModem {
          */
         static void _dispatchEvent(WalterModemCoapEvent event, int profileId);
 #endif
-
-#if CONFIG_WALTER_MODEM_ENABLE
 #pragma endregion
         
 #pragma region MODEM_SLEEP
